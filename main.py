9270dc8d5d48493b76e39cc2eb5e34ddd3e03427
wali.kamp <kamp.wali@gmail.com>
2022-08-19 10:25:30 +0200

    Update 2022-08-19 10:25:30

--- main.py
+++ main.py
@@ -83,160 +83,157 @@ if os.path.exists(r"Output/Bol.com_{}.xlsx".format(datetime.datetime.today().dat
     subcats_links = [link for link in subcats_links if link not in existing_links]
 
 # run through subcategories to get products
-for subcat in tqdm(subcats_links, desc="Subcategories", position=0):
-    driver.get('https://bol.com{}'.format(subcat))
+try:
+    for subcat in tqdm(subcats_links, desc="Subcategories", position=0):
+        driver.get(subcat)
 
-    cat1 = driver.find_elements(By.XPATH, '//ul[@data-test="breadcrumb"]/li')[1].text if \
-        driver.find_elements(By.XPATH, '//ul[@data-test="breadcrumb"]/li') else "NA"
-    cat2 = driver.find_elements(By.XPATH, '//ul[@data-test="breadcrumb"]/li')[2].text if \
-        len(driver.find_elements(By.XPATH, '//ul[@data-test="breadcrumb"]/li')) > 2 else "NA"
-    cat3 = driver.find_elements(By.XPATH, '//ul[@data-test="breadcrumb"]/li')[3].text if \
-        len(driver.find_elements(By.XPATH, '//ul[@data-test="breadcrumb"]/li')) > 3 else "NA"
-    cat4 = driver.find_elements(By.XPATH, '//ul[@data-test="breadcrumb"]/li')[4].text if \
-        len(driver.find_elements(By.XPATH, '//ul[@data-test="breadcrumb"]/li')) > 4 else "NA"
+        cat1 = driver.find_elements(By.XPATH, '//ul[@data-test="breadcrumb"]/li')[1].text if \
+            driver.find_elements(By.XPATH, '//ul[@data-test="breadcrumb"]/li') else "NA"
+        cat2 = driver.find_elements(By.XPATH, '//ul[@data-test="breadcrumb"]/li')[2].text if \
+            len(driver.find_elements(By.XPATH, '//ul[@data-test="breadcrumb"]/li')) > 2 else "NA"
+        cat3 = driver.find_elements(By.XPATH, '//ul[@data-test="breadcrumb"]/li')[3].text if \
+            len(driver.find_elements(By.XPATH, '//ul[@data-test="breadcrumb"]/li')) > 3 else "NA"
+        cat4 = driver.find_elements(By.XPATH, '//ul[@data-test="breadcrumb"]/li')[4].text if \
+            len(driver.find_elements(By.XPATH, '//ul[@data-test="breadcrumb"]/li')) > 4 else "NA"
 
-    # iterate through pages
-    for page in range(1, 2):
-        if page != 1:
-            try:
-                driver.get('https://bol.com{0}?page={1}'.format(subcat, page))
-            except Exception:
-                traceback.print_exc()
-                pass
+        # iterate through pages
+        for page in range(1, 2):
+            if page != 1:
+                try:
+                    driver.get('https://bol.com{0}?page={1}'.format(subcat, page))
+                except Exception:
+                    traceback.print_exc()
+                    pass
 
-        # iterate through products
-        for i in range(len(driver.find_elements(By.XPATH, '//ul[@data-test="products"]/li'))):
-            product = driver.find_elements(By.XPATH, '//ul[@data-test="products"]/li')[i]
-            soup = BeautifulSoup(product.get_attribute('innerHTML'), 'html.parser')
+            # iterate through products
+            for i in range(len(driver.find_elements(By.XPATH, '//ul[@data-test="products"]/li'))):
+                product = driver.find_elements(By.XPATH, '//ul[@data-test="products"]/li')[i]
+                soup = BeautifulSoup(product.get_attribute('innerHTML'), 'html.parser')
 
-            if soup.find('span', attrs={'class': 'product-seller__name'}):
-                seller = soup.find('span', attrs={'class': 'product-seller__name'}).text
-            elif soup.find('div', attrs={'data-test': 'plazaseller-link'}):
-                seller = soup.find('div',
-                                   attrs={'data-test': 'plazaseller-link'}).text.strip().replace('Verkoop door ', '')
-            else:
-                seller = 'NA'
+                if soup.find('span', attrs={'class': 'product-seller__name'}):
+                    seller = soup.find('span', attrs={'class': 'product-seller__name'}).text
+                elif soup.find('div', attrs={'data-test': 'plazaseller-link'}):
+                    seller = soup.find('div',
+                                       attrs={'data-test': 'plazaseller-link'}).text.strip().replace('Verkoop door ', '')
+                else:
+                    seller = 'NA'
 
-            soup.findAll('div')
+                product = {
+                    'date': date,
+                    'week': week,
+                    'timestamp': datetime.datetime.now().time(),
+                    'cat1': cat1,
+                    'cat2': cat2,
+                    'cat3': cat3,
+                    'cat4': cat4 if cat4 else "NA",
+                    'cat_link': 'https://bol.com{}'.format(subcat),
+                    'page': page,
+                    'position': i,
+                    'id': product.get_attribute('data-id'),
+                    'seller': seller,
+                    'brand': soup.find('a', attrs={'data-test': 'party-link'}).text if soup.find('a', attrs={
+                        'data-test': 'party-link'}) else "NA",
+                    'name': soup.find('a', attrs={'data-test': 'product-title'}).text if soup.find('a', attrs={
+                        'data-test': 'product-title'}) else "NA",
+                    'sponsored': True if soup.find('div', attrs={'data-test': 'sponsored-product'}) else False,
+                    'link': soup.find('a', attrs={'data-test': 'product-title'})['href'] if soup.find('a', attrs={
+                        'data-test': 'product-title'}).has_attr('href') else "NA",
+                    'rating': soup.find('div', attrs={'data-test': 'rating-stars'})['title'].split(' ')[1] if soup.find(
+                        'div', attrs={'data-test': 'rating-stars'}) else "NA",
+                    'reviews': (soup.find('div',
+                                          attrs={'data-test': 'rating-stars'})['data-count'] if soup.find('div', attrs={
+                        'data-test': 'rating-stars'}).has_attr('data-count') else "NA") if soup.find('div', attrs={
+                        'data-test': 'rating-stars'}) else "NA",
+                    'delivery': "".join(soup.find('div', attrs={'data-test': 'delivery-notification'}).findAll(text=True,
+                                                                                                               recursive=False)).strip() if soup.find(
+                        'div', attrs={'data-test': 'delivery-notification'}) else "NA",
+                    'price': soup.find('span', attrs={'data-test': 'price'}).text if soup.find('span', attrs={
+                        'data-test': 'price'}) else "NA",
+                    'price_original': soup.find('del', attrs={'data-test': 'from-price'}).text if soup.find('del', attrs={
+                        'data-test': 'from-price'}) else "NA"
+                }
 
-            product = {
-                'date': date,
-                'week': week,
-                'timestamp': datetime.datetime.now().time(),
-                'cat1': cat1,
-                'cat2': cat2,
-                'cat3': cat3,
-                'cat4': cat4 if cat4 else "NA",
-                'cat_link': 'https://bol.com{}'.format(subcat),
-                'page': page,
-                'position': i,
-                'id': product.get_attribute('data-id'),
-                'seller': seller,
-                'brand': soup.find('a', attrs={'data-test': 'party-link'}).text if soup.find('a', attrs={
-                    'data-test': 'party-link'}) else "NA",
-                'name': soup.find('a', attrs={'data-test': 'product-title'}).text if soup.find('a', attrs={
-                    'data-test': 'product-title'}) else "NA",
-                'sponsored': True if soup.find('div', attrs={'data-test': 'sponsored-product'}) else False,
-                'link': soup.find('a', attrs={'data-test': 'product-title'})['href'] if soup.find('a', attrs={
-                    'data-test': 'product-title'}).has_attr('href') else "NA",
-                'rating': soup.find('div', attrs={'data-test': 'rating-stars'})['title'].split(' ')[1] if soup.find(
-                    'div', attrs={'data-test': 'rating-stars'}) else "NA",
-                'reviews': (soup.find('div',
-                                      attrs={'data-test': 'rating-stars'})['data-count'] if soup.find('div', attrs={
-                    'data-test': 'rating-stars'}).has_attr('data-count') else "NA") if soup.find('div', attrs={
-                    'data-test': 'rating-stars'}) else "NA",
-                'delivery': "".join(soup.find('div', attrs={'data-test': 'delivery-notification'}).findAll(text=True,
-                                                                                                           recursive=False)).strip() if soup.find(
-                    'div', attrs={'data-test': 'delivery-notification'}) else "NA",
-                'price': soup.find('span', attrs={'data-test': 'price'}).text if soup.find('span', attrs={
-                    'data-test': 'price'}) else "NA",
-                'price_original': soup.find('del', attrs={'data-test': 'from-price'}).text if soup.find('del', attrs={
-                    'data-test': 'from-price'}) else "NA"
-            }
+                product['price'] = product['price'].replace('\n  ', '.').strip()
+                products = pd.concat([products, pd.Series(product).to_frame().T], ignore_index=True)
 
-            product['price'] = product['price'].replace('\n  ', '.').strip()
-            products = pd.concat([products, pd.Series(product).to_frame().T], ignore_index=True)
+            # remember current tab
+            current_window = driver.current_window_handle
 
-        # remember current tab
-        current_window = driver.current_window_handle
+            # add products to basket
+            links_to_baskets = [product.get_attribute('href') for product in
+                                driver.find_elements(By.XPATH, '//a[@data-test="add-to-basket"]')]
 
-        # add products to basket
-        links_to_baskets = [product.get_attribute('href') for product in
-                            driver.find_elements(By.XPATH, '//a[@data-test="add-to-basket"]')]
+            for product in links_to_baskets:
+                try:
+                    driver.execute_script('window.open(arguments[0]);', product)
+                except Exception:
+                    traceback.print_exc()
+                    pass
 
-        for product in links_to_baskets:
-            try:
-                driver.execute_script('window.open(arguments[0]);', product)
-            except Exception:
-                traceback.print_exc()
-                pass
+            # close tabs
+            for handle in driver.window_handles[::-1]:
+                driver.switch_to.window(handle)
+                if handle != current_window:
+                    driver.close()
 
-        # close tabs
-        for handle in driver.window_handles[::-1]:
-            driver.switch_to.window(handle)
-            if handle != current_window:
-                driver.close()
-
-        # return to original tab
-        driver.switch_to.window(current_window)
-
-        # get product stock from order basket
-        driver.get('https://www.bol.com/nl/order/basket.html')
-
-        # iterate through products and set order quantity to maximum
-        for link in driver.find_elements(By.NAME, 'id'):
-            driver.execute_script('window.open(arguments[0]);',
-                                  'https://www.bol.com/nl/order/basket/updateItemQuantity.html?id={}&quantity=500'.format(
-                                      link.get_attribute('value')))
-            driver.switch_to.window(driver.window_handles[-1])
-            if driver.current_window_handle != current_window:
-                driver.close()
+            # return to original tab
             driver.switch_to.window(current_window)
 
-        # reload
-        driver.get('https://www.bol.com/nl/order/basket.html')
+            # get product stock from order basket
+            driver.get('https://www.bol.com/nl/order/basket.html')
 
-        # get product stocks
-        product_stock = []
-        for product in driver.find_elements(By.XPATH, '//div[@class="product"]'):
-            soup = BeautifulSoup(product.get_attribute('innerHTML'), 'html.parser')
+            # iterate through products and set order quantity to maximum
+            for link in driver.find_elements(By.NAME, 'id'):
+                driver.execute_script('window.open(arguments[0]);',
+                                      'https://www.bol.com/nl/order/basket/updateItemQuantity.html?id={}&quantity=500'.format(
+                                          link.get_attribute('value')))
+                driver.switch_to.window(driver.window_handles[-1])
+                if driver.current_window_handle != current_window:
+                    driver.close()
+                driver.switch_to.window(current_window)
 
-            output = {
-                'link': soup.find('a', {'class': 'product-details__title'})['href'] if soup.find('a', {
-                    'class': 'product-details__title'}) else "NA",
-                'stock': soup.find('option', {'selected': 'selected'}).text if soup.find('option',
-                                                                                         {
-                                                                                             'selected': 'selected'}) else 1
-            }
+            # reload
+            driver.get('https://www.bol.com/nl/order/basket.html')
 
-            products.loc[products['link'] == output['link'], 'stock'] = output['stock']
+            # get product stocks
+            product_stock = []
+            for product in driver.find_elements(By.XPATH, '//div[@class="product"]'):
+                soup = BeautifulSoup(product.get_attribute('innerHTML'), 'html.parser')
 
-        # remove all products from basket
-        for product in driver.find_elements(By.XPATH, '//a[@data-test="remove-link-large"]'):
-            driver.execute_script('window.open(arguments[0]);', product.get_attribute('href'))
-            driver.switch_to.window(driver.window_handles[-1])
-            if driver.current_window_handle != current_window:
-                driver.close()
-            driver.switch_to.window(current_window)
+                output = {
+                    'link': soup.find('a', {'class': 'product-details__title'})['href'] if soup.find('a', {
+                        'class': 'product-details__title'}) else "NA",
+                    'stock': soup.find('option', {'selected': 'selected'}).text if soup.find('option',
+                                                                                             {
+                                                                                                 'selected': 'selected'}) else 1
+                }
 
-print("test")
+                products.loc[products['link'] == output['link'], 'stock'] = output['stock']
+
+            # remove all products from basket
+            for product in driver.find_elements(By.XPATH, '//a[@data-test="remove-link-large"]'):
+                driver.execute_script('window.open(arguments[0]);', product.get_attribute('href'))
+                driver.switch_to.window(driver.window_handles[-1])
+                if driver.current_window_handle != current_window:
+                    driver.close()
+                driver.switch_to.window(current_window)
+except Exception:
+    print("Error occurred while getting products: {}".format(Exception))
+
+print('Done')
 products.to_excel('Bol.com_{}.xlsx'.format(datetime.datetime.today().date()))
+print('Pushed to Excel')
 
-PATH_OF_GIT_REPO = r'.git'  # make sure .git folder is properly configured
-COMMIT_MESSAGE = 'Update'
-remote = f"https://wkampdepuy:{os.getenv('GitHub token')}@github.com/some-account/some-repo.git"
-
-print(datetime.strtime.today())
-datetime
+# push to GitHub
 def git_push():
     try:
         repo = Repo(r'.git')
         repo.git.add(update=True)
-        repo.index.commit('Update {}') # commit message
+        repo.index.commit('Update {}'.format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))) # commit message
         origin = repo.remote(name='origin')
         origin.push()
-        print('Pushed')
-    except:
-        print('Some error occured while pushing the code')
+        print('Pushed to GitHub')
+    except Exception:
+        print(Exception)
 
 git_push()
